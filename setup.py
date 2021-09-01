5f9d7f69b7711c33eb8b627bb43450573bc961ce
Pietro Calixto <pietrocalixto@MacBook-Pro-de-Pietro.local>
2021-09-01 10:38:58 -0300

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='estudando um pouco sobre mlops',
+    author='pietronogueirac',
+    license='',
+)
